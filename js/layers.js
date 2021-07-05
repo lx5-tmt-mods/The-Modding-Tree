2a4fac0fb4421f478b01a553758f503eebfd8524
lastata <lx5.github@yandex.com>
2021-07-05 20:52:57 +0300

    cums are nsfw

--- js/layers.js
+++ js/layers.js
@@ -1,6 +1,6 @@
-addLayer("c", {
-    name: "Cums", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "C", // This appears on the layer's node. Default is the id with the first letter capitalized
+addLayer("f", {
+    name: "Fine guys", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "F", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
@@ -8,7 +8,7 @@ addLayer("c", {
     }},
     color: "#4BDC13",
     requires: new Decimal(5), // Can be a function that takes requirement increases into account
-    resource: "cum", // Name of prestige currency
+    resource: "fine guys", // Name of prestige currency
     baseResource: "guys", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
@@ -19,13 +19,11 @@ addLayer("c", {
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
-        exp = new Decimal(1)
-        if (hasUpgrade('c', 12)) exp = exp.times(2)
-        return exp
+        return new Decimal(1)
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "c", description: "c: Reset for cums", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "f", description: "f: Reset for fine guys", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     upgrades: {
         11: {
@@ -34,9 +32,9 @@ addLayer("c", {
             cost: new Decimal(2),        
         },
         12: {
-            name = "2",
-            description = "^2 your gain",
-            cost = new Decimal(5),
+            name: "2",
+            description: "^2 your gain",
+            cost: new Decimal(5),
             effect() {
                 return player[this.layer].points.add(1).pow(0.5)
             },
